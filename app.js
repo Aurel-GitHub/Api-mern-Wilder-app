196ffbdcaf0b26ca9c7462c9f7424ca737016e90
aurel91000 <56591961+aurel91000@users.noreply.github.com>
2020-11-09 18:09:41 +0100

    GET POST PUT DELETE

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const Wilder = require('./models/wilder');
-// const wilder = require('./models/wilder');
+const wilder = require('./models/wilder');
 
 //connection à la db Mongo sur le cloud
-
 mongoose.connect('mongodb+srv://samepassword:samepassword@cluster0.mppsp.mongodb.net/<dbname>?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -36,10 +34,19 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 /**
+ * GET{id}
+ */
+app.get('/api/wilder/:name', (req, res) => {
+  Wilder.findOne({ name: req.params.name })
+  .then(wilder => res.status(200).json(wilder))
+  .catch(error => res.status(404).json({ error }));
+});
+
+/**
  * POST
  * on passe l'ensemble du contenu du model via l'opérateur spray . . .
  */
-app.post('/api/wilder', (req, res, next) => {
+app.post('/api/wilder', (req, res) => {
     //suppression de l'id du corp de la requete pour eviter pb coté front
     delete req.body._id;  
    const wilder = new Wilder({  
@@ -52,33 +59,34 @@ app.post('/api/wilder', (req, res, next) => {
 });
 
 /**
- * GET{id}
+ * PUT
  */
-app.get('/api/wilder/:name', (req, res, next) => {
-  Wilder.findOne({ name: req.params.name })
-  .then(wilder => res.status(200).json(wilder))
-  .catch(error => res.status(404).json({ error }));
+app.put('/api/wilder/:_id', (req, res) => {
+  Wilder.updateOne({ _id: req.params._id }, {...req.body, _id: req.params._id })
+    .then(() => res.status(200).json( {message: 'Objet modifié !'} ))
+    .catch(error => res.status(400).json({ error }));
 });
+
 
 /**
  * GET
  */
-app.get('/api/wilder', (req, res, next) => {
+app.get('/api/wilder', (req, res) => {
     Wilder.find()
       .then(result => res.status(200).json(result))
       .catch(error => res.status(400).json({error}));
 })
 
-
 /**
- * PUT{id}
+ * DELETE
  */
-app.put('/api/wilder/:id', (req, res, next) => {
-  Wilder.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
-  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
-  .catch(error => res.status(400).json({ error }));
+app.delete('/api/wilder/:_id', (req, res) => {
+  Wilder.deleteOne({ _id: req.params._id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 
 //on exporte l'app pour qu'elle devienne accessible sur l'ensemble du projet
 module.exports = app;
+
