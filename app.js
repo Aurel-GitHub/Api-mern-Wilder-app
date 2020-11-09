eaee32eb647560e449756ffa557c75ba5d27e8b4
aurel91000 <56591961+aurel91000@users.noreply.github.com>
2020-11-09 16:53:54 +0100

    fix GET

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Wilder = require('./models/wilder');
-const wilder = require('./models/wilder');
+// const wilder = require('./models/wilder');
 
 //connection à la db Mongo sur le cloud
 
 mongoose.connect('mongodb+srv://samepassword:samepassword@cluster0.mppsp.mongodb.net/<dbname>?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .then(() => console.log('U are connected MongoDB enjoy !!!'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 
@@ -52,13 +52,32 @@ app.post('/api/wilder', (req, res, next) => {
 });
 
 /**
+ * GET{id}
+ */
+app.get('/api/wilder/:name', (req, res, next) => {
+  Wilder.findOne({ name: req.params.name })
+  .then(wilder => res.status(200).json(wilder))
+  .catch(error => res.status(404).json({ error }));
+});
+
+/**
  * GET
  */
-app.use('/api/wilder', (req, res, next) => {
+app.get('/api/wilder', (req, res, next) => {
     Wilder.find()
-      .then(wilders => res.status(200).json(wilders))
+      .then(result => res.status(200).json(result))
       .catch(error => res.status(400).json({error}));
 })
+
+
+/**
+ * PUT{id}
+ */
+app.put('/api/wilder/:id', (req, res, next) => {
+  Wilder.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
+  .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+  .catch(error => res.status(400).json({ error }));
+});
 
 
 //on exporte l'app pour qu'elle devienne accessible sur l'ensemble du projet
