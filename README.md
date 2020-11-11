07ee7f33810ab0ebd76ce140425a097878c42c2e
aurel91000 <56591961+aurel91000@users.noreply.github.com>
2020-11-12 00:39:47 +0100

    User model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 "# Api-mern-Wilder-app" 
+
+npm i for install and nodemon server to launch and enjoy 💪
---
+++ models/User.js
@@ -0,0 +1,13 @@
+    const mongoose = require('mongoose');
+    const uniqueValidator = require('mongooser');
+
+    //unique pour rendre l'email d'inscription unique
+    const userShema = mongoose.Schema({
+        email: {type: String, required: true, unique: true},
+        password: {type: String, required: true}
+    });
+
+    //application du validator avant d'en faire un model
+    userShema.plugin(uniqueValidator);
+
+    module.export = mongoose.model('User', userShema);
