56d226e59c2189b52e5f408ae8bdb8f84913ada3
aurel91000 <56591961+aurel91000@users.noreply.github.com>
2020-11-12 18:38:51 +0100

    login fix

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,10 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
 
 //middleware pour l'authentification    
-exports.signup = (req, res, next) => {
+exports.signup = (req, res, next) => {  
     bcrypt.hash(req.body.password, 10) //10 le salt qui indique cmb de fois on excute l'algo de hachage
         .then(hash => {
             const user = new User({
@@ -17,5 +19,27 @@ exports.signup = (req, res, next) => {
 };  
 
 exports.login = (req, res, next) => {
-
-};
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          return res.status(401).json({ error: 'Utilisateur non trouvé !' }); //401=non autorisé
+        }
+        bcrypt.compare(req.body.password, user.password) //comparaison entre le password du body et celui haché
+          .then(valid => {
+            if (!valid) {
+              return res.status(401).json({ error: 'Mot de passe incorrect !' });
+            }
+            res.status(200).json({
+              userId: user._id, 
+              //sign() prend en arg un payload données encodés
+              token: jwt.sign(
+                { userId: user._id }, //encodage de l'id pour eviter qu'un autre utilisateur modifie l'objet par crée
+                'RANDOM_TOKEN_SECRET', //mdp pour la partie dev à remplacer en prod par une chaine de caract.
+                { expiresIn: '24h' } //chaque token dure 24h au delà de 24h il n'est plus valable
+              )
+            });
+          })
+          .catch(error => res.status(500).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error })); //message d'erreur si il y a un pb de co à MongoDB
+  };
